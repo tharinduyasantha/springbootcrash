1a6b92dd536ae22a64a2aa77fad6bcff7b7a91a5
TechnicalDifficulty <yasanthatharindu.qs@gmail.com>
2019-12-16 22:40:51 +0530

    springboot crash project initialized.

---
+++ src/main/java/com/technicaldifficulty/springbootcrash/SpringbootcrashApplication.java
@@ -0,0 +1,13 @@
+package com.technicaldifficulty.springbootcrash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcrashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technicaldifficulty/springbootcrash/SpringbootcrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicaldifficulty.springbootcrash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
